9cbcc69ab8c223325923a18d687e745462585e63
Lucy Carroll <lucyc@live.unc.edu>
2022-01-19 12:57:58 -0500

    progress on ex01

---
+++ exercises/ex01_chardle.py
@@ -0,0 +1,18 @@
+"""EX01 - Chardle - A cute step toward Wordle."""
+__author__ = "730320310" 
+
+wordle_word: str = input("Enter a 5-character word: ")
+single_character: str = input("Enter a single character: ")
+print("Searching for e in hello")
+print("e found at index 1")
+
+wordle_word: str = input("Enter a 5 character word: ")
+single_character: str = input("Enter a single character: ")
+print("Searching for e in heels")
+print("e found at index 1")
+print("e found at index 2")
+
+wordle_word: str = input("Enter a 5-character word: ")
+single_character: str = input("Enter a single character: ")
+print("Searching for s in heels")
+print("s found at index 4")
