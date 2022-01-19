87f7bfa4069366eb2d0d79317d0d3ca2c5cce072
Lucy Carroll <lucyc@live.unc.edu>
2022-01-19 16:45:51 -0500

    more progress ex01

--- exercises/ex01_chardle.py
+++ exercises/ex01_chardle.py
@@ -1,18 +1,43 @@
 """EX01 - Chardle - A cute step toward Wordle."""
 __author__ = "730320310" 
 
-wordle_word: str = input("Enter a 5-character word: ")
-single_character: str = input("Enter a single character: ")
+word: str = input("Enter a 5-character word: ")
+character: str = input("Enter a single character: ")
 print("Searching for e in hello")
-print("e found at index 1")
 
-wordle_word: str = input("Enter a 5 character word: ")
-single_character: str = input("Enter a single character: ")
+if character == "hello"[1]:
+    print("e found at index 1")
+else:
+    character == "hello"[0]
+    character == "hello"[2]
+    character == "hello"[3]
+    character == "hello"[4]
+    print("letter not found at index 1")
+
+
+
+word: str = input("Enter a 5 character word: ")
+character: str = input("Enter a single character: ")
 print("Searching for e in heels")
-print("e found at index 1")
-print("e found at index 2")
 
-wordle_word: str = input("Enter a 5-character word: ")
-single_character: str = input("Enter a single character: ")
+if character == "heels"[1]:  
+    print("e found at index 1")
+    if character == "heels"[2]: 
+        print("e found at index 2")
+else: 
+    character != "heels"[1]
+    print("letter not found at index 1")
+    character != "heels"[2]
+    print("letter not found at index 2")
+    
+
+
+word: str = input("Enter a 5-character word: ")
+character: str = input("Enter a single character: ")
 print("Searching for s in heels")
-print("s found at index 4")
+
+if  character == "heels"[4]:
+    print("s found at index 4")
+else: 
+    character != "heels"[4]
+    print("letter not found at index 4")
