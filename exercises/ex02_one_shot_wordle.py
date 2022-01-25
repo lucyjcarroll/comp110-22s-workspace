6041e359eedbc621c63c6acfff72ca86b9d200f4
Lucy Carroll <lucyc@live.unc.edu>
2022-01-25 16:50:15 -0500

    progress ex02

--- exercises/ex02_one_shot_wordle.py
+++ exercises/ex02_one_shot_wordle.py
@@ -7,10 +7,17 @@ i: int = len(secret_word)
 
 while i < len(guess):
     print(f"That was not {i} letters! Try again: ")
-    i = i + 1
+    i = i + 6
 
+i: int = len(secret_word)
 while i > len(guess):
     print(f"That was not {i} letters! Try again: ")
-    
+    i = i - 6
+
 while i == len(guess) and guess == secret_word:
     print("Woo! You got it!")
+    guess = "hello"
+
+while i == len(guess) and guess != secret_word:
+    print("Not quite. Play again soon!")
+    i = i + 1
