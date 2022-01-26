99355feb4599f4a2c2d527efa5fc40efd383fd1c
Lucy Carroll <lucyc@live.unc.edu>
2022-01-26 10:32:04 -0500

    progress ex02

--- exercises/ex02_one_shot_wordle.py
+++ exercises/ex02_one_shot_wordle.py
@@ -6,31 +6,30 @@ guess: str = input("What is your 6-letter guess? ")
 WHITE_BOX: str = "\U00002B1C"
 GREEN_BOX: str = "\U0001F7E9"
 YELLOW_BOX: str = "\U0001F7E8"
-
-idx = str = [secret_word]
-
+    
+# below is loop for too-long guesses
 i: int = len(secret_word)  
 while i < len(guess):
-    print(f"That was not {i} letters! Try again: ")
-    i = i + 6
+    next_guess: str = input(f"That was not {len(secret_word)} letters! Try again: ") 
+    i = i + len(secret_word)
+
+# below is loop for too-short guesses 
 i: int = len(secret_word)
 while i > len(guess):
+    next_guess: str = input(f"That was not {len(secret_word)} letters! Try again: ") 
     print(f"That was not {i} letters! Try again: ")
-    i = i - 6
+    i = i - len(secret_word)
 
+# below is loop for incorrect guesses with correct length 
 i: int = len(secret_word)
 while i == len(guess) and guess != secret_word:
-   
-    while idx == [guess]:
-        print(GREEN_BOX)
-        idx != [guess]
-
     print("Not quite. Play again soon!")
     i = i + 1
 
+# below is loop for correct guesses
 i: int = len(secret_word)
 while i == len(guess) and guess == secret_word:
-    print(f"{GREEN_BOX}{GREEN_BOX}{GREEN_BOX}{GREEN_BOX}{GREEN_BOX}{GREEN_BOX}")  # no need to check indices because if secret_word == guess, it will always be correct to print 6 green boxes. 
+    print(f"{GREEN_BOX}" * len(secret_word))  
     print("Woo! You got it!")
     guess = "hello"  # this is a word that would never be chosen as a secret word because it's not 6 letters
     i != len(secret_word)
