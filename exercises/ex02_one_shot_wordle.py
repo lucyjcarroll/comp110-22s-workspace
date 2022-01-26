55fb831778085ed3f0976739f510747c8075a68b
Lucy Carroll <lucyc@live.unc.edu>
2022-01-26 13:04:08 -0500

    progress ex02

--- exercises/ex02_one_shot_wordle.py
+++ exercises/ex02_one_shot_wordle.py
@@ -6,30 +6,24 @@ guess: str = input("What is your 6-letter guess? ")
 WHITE_BOX: str = "\U00002B1C"
 GREEN_BOX: str = "\U0001F7E9"
 YELLOW_BOX: str = "\U0001F7E8"
-    
-# below is loop for too-long guesses
-i: int = len(secret_word)  
-while i < len(guess):
-    next_guess: str = input(f"That was not {len(secret_word)} letters! Try again: ") 
-    i = i + len(secret_word)
 
-# below is loop for too-short guesses 
-i: int = len(secret_word)
-while i > len(guess):
-    next_guess: str = input(f"That was not {len(secret_word)} letters! Try again: ") 
-    print(f"That was not {i} letters! Try again: ")
-    i = i - len(secret_word)
-
-# below is loop for incorrect guesses with correct length 
-i: int = len(secret_word)
-while i == len(guess) and guess != secret_word:
+i: int = len(guess)
+while i == len(secret_word) and guess != secret_word:
     print("Not quite. Play again soon!")
     i = i + 1
 
+# below loop is for guesses with incorrect length
+i: int = len(guess)
+while i != len(secret_word):
+    wrong_guess = input(f"That was not {len(secret_word)} letters! Try again: ")
+    if len(wrong_guess) == len(secret_word) and wrong_guess != secret_word:
+        print("Not quite. Play again soon!")
+        i = len(secret_word)
+
 # below is loop for correct guesses
-i: int = len(secret_word)
-while i == len(guess) and guess == secret_word:
-    print(f"{GREEN_BOX}" * len(secret_word))  
+i: int = len(guess)
+while i == len(secret_word) and guess == secret_word:
+    print(f"{GREEN_BOX}" * len(guess))  
     print("Woo! You got it!")
     guess = "hello"  # this is a word that would never be chosen as a secret word because it's not 6 letters
     i != len(secret_word)
