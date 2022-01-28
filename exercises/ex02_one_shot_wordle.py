97ad11738e91118b5f9c2c1cef3b602d7863685b
Lucy Carroll <lucyc@live.unc.edu>
2022-01-28 16:13:05 -0500

    progress

--- exercises/ex02_one_shot_wordle.py
+++ exercises/ex02_one_shot_wordle.py
@@ -1,28 +1,37 @@
 """Second Try on Wordle- EX02."""
 __author__ = "730320310"
 
-secret_word = str("python")
-guess: str = input(f"What is your {len(secret_word)}-letter guess? ")
+from operator import index
+
+
+secret = str("python")
+guess: str = input(f"What is your {len(secret)}-letter guess? ")
 WHITE_BOX: str = "\U00002B1C"
 GREEN_BOX: str = "\U0001F7E9"
 YELLOW_BOX: str = "\U0001F7E8"
 
-idx = len(secret_word) - 1
-emoji_str = ""
+idx = len(secret) - len(guess)
+emoji = ""
+i = [index]
 
+while idx < len(secret) and len(secret) == len(guess):
+    if [guess] == [secret]:
+        emoji = emoji + f"{GREEN_BOX}"
+        idx = idx + 1
+    elif [guess] != [secret]:
+        emoji = emoji + f"{WHITE_BOX}"
+        idx = idx + 1
+    print(emoji)
 
-i: int = len(guess)  # loop for correct length, wrong guess 
-
-while i != len(secret_word):
-    wrong_guess = input(f"That was not {len(secret_word)} letters! Try again: ")
-    if len(wrong_guess) == len(secret_word) and wrong_guess != secret_word:
-        i = len(secret_word)
-            
-# loop for correct length, wrong guess 
-while i == len(secret_word) and guess != secret_word:
+guess_len: int = len(guess)  
+while guess_len != len(secret): 
+    guess_again = input(f"That was not {len(secret)} letters! Try again: ")
+    if len(guess_again) == len(secret) and guess_again != secret:
+        guess_len = len(secret)
+     
+while guess_len == len(secret) and guess != secret:  # loop for correct length, wrong guess
     print("Not quite. Play again soon!")
-    i = i + 1
-while i == len(secret_word) and guess == secret_word:
-    print(f"{GREEN_BOX}" * len(secret_word))  
+    guess_len = guess_len + 1
+while guess_len == len(secret) and guess == secret:  # loop for correct guess  
     print("Woo! You got it!")
-    i = i + 1
+    guess_len = guess_len + 1
