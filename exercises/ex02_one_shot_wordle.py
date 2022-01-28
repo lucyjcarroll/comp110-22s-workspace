20ec22a750d411a095123700626ab4c1be136946
Lucy Carroll <lucyc@live.unc.edu>
2022-01-28 08:57:36 -0500

    progress

--- exercises/ex02_one_shot_wordle.py
+++ exercises/ex02_one_shot_wordle.py
@@ -10,19 +10,23 @@ YELLOW_BOX: str = "\U0001F7E8"
 idx = len(secret_word) - 1
 emoji_str = ""
 
-i: int = len(guess)  # loop for correct length, wrong guess 
-while i == len(secret_word) and guess != secret_word:
-    print("Not quite. Play again soon!")
-    i = i + 1
 
-# loop for correct length, wrong guess 
+i: int = len(guess)  # loop for correct length, wrong guess 
 while i != len(secret_word):
     wrong_guess = input(f"That was not {len(secret_word)} letters! Try again: ")
     if len(wrong_guess) == len(secret_word) and wrong_guess != secret_word:
         print("Not quite. Play again soon!")
         i = len(secret_word)
+    else:
+        if wrong_guess == secret_word:
+            print(f"{GREEN_BOX}" * len(secret_word))
+            print("Woo! You got it!")
+        exit()
     
- 
+i: int = len(guess)  # loop for correct length, wrong guess 
+while i == len(secret_word) and guess != secret_word:
+    print("Not quite. Play again soon!")
+    i = i + 1 
 while i == len(secret_word) and guess == secret_word:
     print(f"{GREEN_BOX}" * len(secret_word))  
     print("Woo! You got it!")
