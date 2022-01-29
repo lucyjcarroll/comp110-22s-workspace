0ea1aaf3c5242f46a322349c54daa32887a4b829
Lucy Carroll <lucyc@live.unc.edu>
2022-01-29 12:23:41 -0500

    progress

--- exercises/ex02_one_shot_wordle.py
+++ exercises/ex02_one_shot_wordle.py
@@ -1,34 +1,31 @@
 """Second Try on Wordle- EX02."""
 __author__ = "730320310"
 
-from operator import index
-
-
 secret = str("python")
 guess: str = input(f"What is your {len(secret)}-letter guess? ")
 WHITE_BOX: str = "\U00002B1C"
 GREEN_BOX: str = "\U0001F7E9"
 YELLOW_BOX: str = "\U0001F7E8"
 
-idx = len(secret) - len(guess)
+idx = 0
 emoji = ""
-i = [index]
 
-while idx < len(secret) and len(secret) == len(guess):
-    if [guess] == [secret]:
+while idx < len(secret):
+    if guess[idx] == secret[idx]:
         emoji = emoji + f"{GREEN_BOX}"
         idx = idx + 1
-    elif [guess] != [secret]:
+    elif guess[idx] != secret[idx]:
         emoji = emoji + f"{WHITE_BOX}"
         idx = idx + 1
-    print(emoji)
+print(emoji)
+
 
 guess_len: int = len(guess)  
 while guess_len != len(secret): 
     guess_again = input(f"That was not {len(secret)} letters! Try again: ")
     if len(guess_again) == len(secret) and guess_again != secret:
         guess_len = len(secret)
-     
+
 while guess_len == len(secret) and guess != secret:  # loop for correct length, wrong guess
     print("Not quite. Play again soon!")
     guess_len = guess_len + 1
