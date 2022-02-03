9d4741d9033f9008c1919494214e40ef009a43b7
Lucy Carroll <lucyc@live.unc.edu>
2022-02-02 20:23:11 -0500

    progress

--- exercises/ex03_wordle.py
+++ exercises/ex03_wordle.py
@@ -1,15 +1,38 @@
 """EX03: Structured Wordle"""
 __author__ = "730320310"
 
+WHITE_BOX: str = "\U00002B1C"
+GREEN_BOX: str = "\U0001F7E9"
+YELLOW_BOX: str = "\U0001F7E8"
 
-def contains_char(secret: str, used_char: str) -> bool:
-    """Function to search guess for characters that are present in secret word and returns True/False."""
-    assert len(used_char) == 1
+
+def contains_char(word: str, char: str) -> bool:
+    """Function to search guess for characters that are 
+    present in secret word and returns True/False."""
+    assert len(char) == 1
     idx: int = 0
-    while idx < len(secret):
-        if secret[idx] == used_char:
+    while idx < len(word):
+        if word[idx] == char:
             return True
         else:
             idx = idx + 1
     return False 
-            
+
+
+def emojified(guess: str, secret: str) -> str: 
+    """Function to print emojis(white, yellow, green boxes)
+     to signal if characters match/do not match/are contained in the word."""
+    assert len(guess) == len(secret)
+    idx: int = 0
+    emoji: str = ""
+    while idx < len(secret):
+        if secret[idx] == guess[idx]:
+            emoji = emoji + f"{GREEN_BOX}"
+            idx = idx + 1
+        elif contains_char(secret, guess[idx]):
+            emoji = emoji + f"{YELLOW_BOX}"
+            idx = idx + 1
+        elif contains_char is not True:
+            emoji = emoji + f"{WHITE_BOX}"
+            idx = idx + 1
+    return emoji 
