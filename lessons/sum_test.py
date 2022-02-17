051d0c904062335f5618932e24ee7cdf448f25c2
Lucy Carroll <lucyc@live.unc.edu>
2022-02-16 19:04:56 -0500

    work

---
+++ lessons/sum_test.py
@@ -0,0 +1,14 @@
+"""Tests for the sum fxn."""
+
+
+from lessons.sum import sum 
+
+
+def test_sum_empty() -> None:
+    xs: list[float] = []
+    assert sum(xs) == 0.0
+
+
+def test_sum_single_item() -> None:
+    xs: list[float] = [110.0]
+    assert sum(xs) == 110.0
