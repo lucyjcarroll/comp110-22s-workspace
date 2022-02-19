0b94122a6719f62d2a26160294c1a6c7ac8e190b
Lucy Carroll <lucyc@live.unc.edu>
2022-02-19 11:52:24 -0500

    progress

--- exercises/ex05/utils_test.py
+++ exercises/ex05/utils_test.py
@@ -1,6 +1,6 @@
 """Exercise 05: Practicing testing on list utility functions."""
 __author__ = "730320310"
-from exercises.ex05.utils import only_evens
+from exercises.ex05.utils import only_evens, concat
 
 
 def test_only_evens_mix() -> None:
@@ -17,4 +17,20 @@ def test_only_evens_even() -> None:
     numbers: list[int] = [4, 4, 4]
     assert only_evens(numbers) == [4, 4, 4]
 
-def test_sub_
+
+def test_concat_2_full_lists() -> None:
+    a_list: list[int] = [2, 2, 2]
+    another_list: list[int] = [1, 1, 1]
+    assert concat(a_list, another_list) == [2, 2, 2, 1, 1, 1]
+
+
+def test_concat_2_empty_lists() -> None:
+    a_list: list[int] = []
+    another_list: list[int] = []
+    assert concat(a_list, another_list) == []
+
+
+def test_concat_1_empty_list() -> None:
+    a_list: list[int] = []
+    another_list: list[int] = [47, 5, 6]
+    assert concat(a_list, another_list) == [47, 5, 6]
