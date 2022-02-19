eeda61c6c94bfd439ad79d3d54ee00d541e6524b
Lucy Carroll <lucyc@live.unc.edu>
2022-02-19 11:52:33 -0500

    progress

--- exercises/ex05/utils.py
+++ exercises/ex05/utils.py
@@ -13,19 +13,21 @@ def only_evens(numbers: list[int]):
         elif numbers[i] % 2 != 0:
             i += 1
     return returned_list
-            
+
+
+def concat(a_list: list[int], another_list: list[int]):
+    """Function to combine 2 lists to create a new one."""
+    merged_list: list[int] = []
+    i: int = 0
+    while i < len(a_list):
+        merged_list.append(a_list[i])
+    while i < len(another_list):
+        merged_list.append(another_list[i])
+    return merged_list
+
 
 def sub(a_list: list[int], start_idx: int, end_idx: int):
     """Function to return a list with only numbers between the start and end index."""
     i: int = 0
-    resulting_list = []
-    if len(a_list) == 0 or start_idx > len(a_list) or end_idx <= 0:
-        return resulting_list
-
-    return resulting_list
-
-
-def concat(a_list: list[int], another_list: list[int]) -> str:
-    """Function to combine 2 lists to create a new one."""
-    empty_string = ""
-    return f"{empty_string} {a_list} {another_list}"
+    if len(a_list) == 0 or start_idx > len(a_list) or end_idx <= 0: 
+        a_list
