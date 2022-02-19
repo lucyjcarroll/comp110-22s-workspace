a67ee5c7bc68f372ab44321004be6f4b9c57d6b9
Lucy Carroll <lucyc@live.unc.edu>
2022-02-18 22:29:40 -0500

    progess

--- exercises/ex05/utils_test.py
+++ exercises/ex05/utils_test.py
@@ -1,3 +1,18 @@
 """Exercise 05: Practicing testing on list utility functions."""
 __author__ = "730320310"
+from exercises.ex05.utils import only_evens
 
+
+def test_only_evens_mix() -> None:
+    xs: list[int] = [3, 1, 2]
+    assert only_evens(xs) == [2]
+
+
+def test_only_evens_odds() -> None:
+    xs: list[int] = [1, 5, 3]
+    assert only_evens(xs) == []
+
+
+def test_only_evens_even() -> None:
+    xs: list[int] = [4, 4, 4]
+    assert only_evens(xs) == [4, 4, 4]
