77c8d25fe09b3bc56c638e50c4056a39a31b270a
Lucy Carroll <lucyc@live.unc.edu>
2022-02-21 14:39:46 -0500

    done

--- exercises/ex05/utils_test.py
+++ exercises/ex05/utils_test.py
@@ -9,6 +9,12 @@ def test_only_evens_mix() -> None:
     assert only_evens(numbers) == [2]
 
 
+def test_only_evens_mix_2() -> None:
+    """Unit test 2 for list with even and odd numbers mixed."""
+    numbers: list[int] = [4, 6, 1]
+    assert only_evens(numbers) == [4, 6]
+
+
 def test_only_evens_odds() -> None:
     """Unit test for list with only odd numbers."""
     numbers: list[int] = [1, 5, 3]
@@ -26,6 +32,13 @@ def test_concat_2_full_lists() -> None:
     a_list: list[int] = [2, 2, 2]
     another_list: list[int] = [1, 1, 1]
     assert concat(a_list, another_list) == [2, 2, 2, 1, 1, 1]
+
+
+def test_concat_2_full_lists_2() -> None:
+    """Unit test  2 for 2 full lists."""
+    a_list: list[int] = [4, 4, 4]
+    another_list: list[int] = [6, 6, 6]
+    assert concat(a_list, another_list) == [4, 4, 4, 6, 6, 6]
 
 
 def test_concat_2_empty_lists() -> None:
@@ -48,19 +61,25 @@ def test_sub_empty() -> None:
     assert sub(a_list, 2, 3) == []
 
 
-def test_sub_test_1() -> None:
+def test_sub_test_out_of_range() -> None:
     """Unit test for end index out of list range."""
     tester_list: list[int] = [5, 6, 7, 8, 9]
     assert sub(tester_list, 0, 10) == [5, 6, 7, 8, 9]
 
 
-def test_sub_test_2() -> None: 
+def test_sub_test_neg_start() -> None: 
     """Unit test for negative start index."""
     a_list: list[int] = [0, 1, 2, 3, 4, 5]
     assert sub(a_list, -1, 4) == [0, 1, 2, 3]
 
 
-def test_sub_test_3() -> None:
-    """Unite test for start and end indexes inside of list (no edge cases."""
+def test_sub_use_1() -> None:
+    """Unit test for start and end indexes inside of list (no edge cases)."""
+    a_list: list[int] = [1, 2, 3, 4]
+    assert sub(a_list, 1, 3) == [2, 3]
+
+
+def test_sub_use_2() -> None:
+    """Unit test 2 for start and end indexes inside of list (no edge cases)."""
     a_list: list[int] = [1, 2, 3, 4]
     assert sub(a_list, 1, 3) == [2, 3]
