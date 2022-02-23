1fd5b079d738f3698648f5560510073020d342d9
Lucy Carroll <lucyc@live.unc.edu>
2022-02-22 19:16:58 -0500

    done

---
+++ lessons/dict.py
@@ -0,0 +1,12 @@
+"""Working with dictionaries."""
+
+schools = {"UNC": 19400, "Dukie": 6717, "NCSU": 26150}
+print(schools)
+
+#  print(schools["UNCC"])
+
+# example looping over the keys of a dict
+
+for key in schools:
+    print(f"Key: {key} -> Value: {schools[key]}")
+
