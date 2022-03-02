bbbb8413e43785be6ed5c46a72e2fe384453f4c8
Lucy Carroll <lucyc@live.unc.edu>
2022-03-02 17:08:07 -0500

    progress

--- exercises/ex06/dictionary.py
+++ exercises/ex06/dictionary.py
@@ -2,10 +2,19 @@
 __author__ = "730320310"
 
 
-def invert(a_dictionary):
-    resulting_dictionary = {}
-    for key, value in a_dictionary:
-        if value not in a_dictionary:
-            resulting_dictionary[value] = []
-        resulting_dictionary[value].append(key)
-        return resulting_dictionary
+def invert(a_dictionary: dict[str, str]) -> dict[str, str]: 
+    inverse_dict = {a_dictionary[key]: key for key in a_dictionary}
+    return inverse_dict
+
+
+def favorite_color(a_dict: dict[str, str]) -> str: 
+    colors = []
+    for color in a_dict:
+        if color in a_dict:
+            colors.append(color)
+    for favorite in colors:
+        if favorite in colors:
+            return favorite_color(colors.count(favorite))
+
+
+def count(a_list: list[str]) -> dict[str, int]:
