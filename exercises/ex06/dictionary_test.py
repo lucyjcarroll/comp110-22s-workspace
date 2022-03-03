63ea96db94ee82359326414a68c34f8b2d04fa6c
Lucy Carroll <lucyc@live.unc.edu>
2022-03-03 15:44:11 -0500

    progress

--- exercises/ex06/dictionary_test.py
+++ exercises/ex06/dictionary_test.py
@@ -1,7 +1,7 @@
 """Unit Tests for Dictionaries: Exercise 6."""
 __author__ = "730320310"
 
-from exercises.ex06.dictionary import invert, favorite_color
+from exercises.ex06.dictionary import invert, favorite_color, count 
 
 
 def test_invert_use_1() -> None:
@@ -32,3 +32,18 @@ def test_favorite_color_use_2() -> None:
 def test_favorite_color_edge() -> None:
     """Edge case for favorite color function."""
     assert favorite_color({}) == ''
+
+
+def test_count_use_1() -> None:
+    """Use case 1 for count function."""
+    assert count(['2', '2', '1']) == {'2': 2, '1': 1}
+
+
+def test_count_use_2() -> None:
+    """Use case 2 for count function."""
+    assert count(['3', '3', '2', '1', '3', '2']) == {'3': 3, '2': 2, '1': 1}
+
+
+def test_count_edge() -> None:
+    """Edge case for count function."""
+    assert count([]) == {}
